42ac2c92d19788eb5f7a37c5f147f75812cc8ccd
Kamil Szymoniak <kamilszmk@gmail.com>
2021-04-16 22:03:17 +0200

    Dodaj grid view dla zdjęć

---
+++ ImageCollection-front/src/model/dto.ts
@@ -0,0 +1,10 @@
+export interface TileImageResponse {
+    thumb: any;
+    title: string;
+    author: string;
+    authorId: number;
+    description: string;
+}
+
+export interface ImageResponse {
+}
