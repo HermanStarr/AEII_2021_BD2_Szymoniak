dfc38237411cfed1b294d262f2a6b258137e21af
vikwil <viktwil638@student.polsl.pl>
2021-05-08 16:41:29 +0200

    swagger fix

--- ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/ImageCollectionApplication.java
+++ ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/ImageCollectionApplication.java
@@ -4,6 +4,7 @@ package pl.polsl.dsa.imagecollection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
