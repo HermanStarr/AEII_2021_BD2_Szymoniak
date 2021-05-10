fb68eb0fcb8ac0684568d83d9f724b52af0af047
Kamil Szymoniak <77056179+HermanStarr@users.noreply.github.com>
2021-05-10 11:28:41 +0200

    Revert "Tag files configuration (#12)"
    This reverts commit 2db154e9286df8ad842cfcbd2deef87e04fdb07d.

--- ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/dao/CategoryRepository.java
+++ ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/dao/CategoryRepository.java
@@ -3,13 +3,9 @@ package pl.polsl.dsa.imagecollection.dao;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import pl.polsl.dsa.imagecollection.model.CategoryEntity;
-import pl.polsl.dsa.imagecollection.model.TagEntity;
-
-import java.util.stream.Stream;
 
 public interface CategoryRepository extends
         CrudRepository<CategoryEntity, Long>,
         JpaSpecificationExecutor<CategoryEntity> {
-    Stream<CategoryEntity> getAll();
-    CategoryEntity findByName (String name);
-    }
+
+}
