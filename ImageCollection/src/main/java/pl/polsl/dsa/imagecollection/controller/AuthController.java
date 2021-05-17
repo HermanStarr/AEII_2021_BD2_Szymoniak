52994163597f4dce08d48d422957e69fe691c8e6
Kamil Żurawlow <kamil.zurawlow@softiq.pl>
2021-05-17 13:50:21 +0200

    Added data retrieving from backend when user is logged in

--- ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/controller/AuthController.java
+++ ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/controller/AuthController.java
@@ -3,19 +3,22 @@ package pl.polsl.dsa.imagecollection.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.polsl.dsa.imagecollection.dao.UserRepository;
 import pl.polsl.dsa.imagecollection.dto.ApiResponse;
 import pl.polsl.dsa.imagecollection.dto.LoginRequest;
 import pl.polsl.dsa.imagecollection.dto.SignUpRequest;
+import pl.polsl.dsa.imagecollection.dto.UserResponse;
 import pl.polsl.dsa.imagecollection.exception.ResourceNotFoundException;
 import pl.polsl.dsa.imagecollection.model.UserEntity;
 import pl.polsl.dsa.imagecollection.security.JwtUtils;
+import pl.polsl.dsa.imagecollection.service.UserDetailsImpl;
+import pl.polsl.dsa.imagecollection.service.UserDetailsServiceImpl;
 import pl.polsl.dsa.imagecollection.service.UserService;
 
 import javax.validation.Valid;
@@ -45,42 +48,50 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse> loginUser(@Valid @RequestBody LoginRequest request) {
-        if(!userRepository.existsByNickname(request.getUsername()))
+        if (!userRepository.existsByNickname(request.getUsername()))
             return ResponseEntity
-                    .badRequest()
-                    .body(new ApiResponse(false,"Error: User does not exist!"));
+                .badRequest()
+                .body(new ApiResponse(false, "Error: User does not exist!"));
 
         UserEntity user = userRepository.findByNickname(request.getUsername())
-                .orElseThrow(() -> new ResourceNotFoundException("User", "nickname", request.getUsername()));
+            .orElseThrow(() -> new ResourceNotFoundException("User", "nickname", request.getUsername()));
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        if ( !encoder.matches(request.getPassword(), userService.byteToString(user.getPasswordHash())) ) {
+        if (!encoder.matches(request.getPassword(), userService.byteToString(user.getPasswordHash()))) {
             return ResponseEntity
-                    .badRequest()
-                    .body(new ApiResponse(false,"Error: Wrong password"));
+                .badRequest()
+                .body(new ApiResponse(false, "Error: Wrong password"));
         }
 
         userService.login(request);
-        return ResponseEntity.ok(new ApiResponse(true,"User logged in successfully!"));
+        return ResponseEntity.ok(new ApiResponse(true, "User logged in successfully!"));
     }
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse> registerUser(@Valid @RequestBody SignUpRequest request) {
-        if(userRepository.existsByNickname(request.getUsername())) {
+        if (userRepository.existsByNickname(request.getUsername())) {
             return ResponseEntity
-                    .badRequest()
-                    .body(new ApiResponse(false,"Error: Username is already taken!"));
+                .badRequest()
+                .body(new ApiResponse(false, "Error: Username is already taken!"));
         }
-        if(userRepository.existsByEmail(request.getEmail())) {
+        if (userRepository.existsByEmail(request.getEmail())) {
             return ResponseEntity
-                    .badRequest()
-                    .body(new ApiResponse(false,"Error: Email is already taken!"));
+                .badRequest()
+                .body(new ApiResponse(false, "Error: Email is already taken!"));
         }
 
         userService.signUp(request);
         return ResponseEntity.ok(
-                new ApiResponse(true,"User registered successfully!"));
+            new ApiResponse(true, "User registered successfully!"));
+    }
+
+    @GetMapping("/userData")
+    public UserResponse getUserData() {
+        UserDetails u = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UserEntity user = userRepository.findByNickname(u.getUsername())
+            .orElseThrow(() -> new ResourceNotFoundException("", "", u.getUsername()));
+        return UserResponse.fromEntity(user);
     }
 
 }
