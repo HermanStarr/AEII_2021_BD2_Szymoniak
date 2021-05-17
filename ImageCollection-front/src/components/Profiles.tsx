06514fe6b3f8db4b4805a419c3877c4403e0e9b1
klaudiaaaj <56549544+klaudiaaaj@users.noreply.github.com>
2021-05-17 10:28:10 +0200

    Profiles table

--- ImageCollection-front/src/components/Profiles.tsx
+++ ImageCollection-front/src/components/Profiles.tsx
@@ -1,12 +1,133 @@
-import {Container} from "@material-ui/core";
-import React from "react";
+import {
+    Container, InputBase,
+    makeStyles,
+    Paper,
+    Table, TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    Theme,
+    TableRow,
+    withStyles
+} from "@material-ui/core";
+import React, {useEffect, useState} from "react";
+import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
+import {TileImageResponse, UserResponse} from "../model/dto";
+import {withRouter} from "react-router";
+import SearchIcon from "@material-ui/icons/Search";
 
 export const Profiles = () => {
+    const classes = useStyles();
+    const [profiles, setprofiles] = useState<UserResponse[]>();
 
+    useEffect(() => {
+        getProfiles().then((response: UserResponse[]) => {
+            setprofiles(response);
+        })
+    }, []);
 
     return (
-      <Container>
-          PLACEHOLDER
-      </Container>
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                    <TableRow >
+                        <StyledTableCell colSpan={2} >
+                        <div>
+                                <SearchIcon/>
+                            <InputBase
+                                placeholder="Search…"
+                                onChange={event => {}}
+                                inputProps={{'aria-label': 'search'}}
+                                classes ={{
+                                   root: classes.inputRoot,
+                                    input: classes.inputInput
+                                }}
+                            />
+                        </div></StyledTableCell>
+                    </TableRow>
+                <TableRow >
+                        <StyledTableCell>Icon</StyledTableCell>
+                        <StyledTableCell >Nick</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {profiles?.map((row) => (
+                        <StyledTableRow key={row.icon}>
+                            <StyledTableCell component="th" scope="row">
+                                <AccessibilityNewIcon></AccessibilityNewIcon>
+                            </StyledTableCell>
+                            <StyledTableCell>{row.nickname}</StyledTableCell>
+                        </StyledTableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+    searchBar:{
+        color: 'white',
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 1),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+}))(TableRow);
+
+const useStyles = makeStyles((theme) =>({
+    table: {
+        minWidth: 700,
+    },
+    full: {
+        width: '200%' ,
+        span: 2,
+},
+    inputRoot: {
+        color: 'inherit',
+    },
+inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+}));
+const getProfiles = async (): Promise<UserResponse[]> => {
+    {
+        return [
+            {id:1, icon:undefined, nickname:"Nick_1", email:"some@mail.com"},
+            {id:2, icon:undefined, nickname:"Nick_2", email:"some2@mail.com"},
+            {id:3, icon:undefined, nickname:"Nick_3", email:"some3@mail.com"},
+        ];
+    };
+    }
+
+    export default withRouter(Profiles);
