f22f89c72cbbe69a64a4d11368b551350cb03925
HermanStarr <mcmamutant@gmail.com>
2021-06-14 12:04:06 +0200

    Change image repository.

--- ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/dao/ImageRepository.java
+++ ImageCollection/src/main/java/pl/polsl/dsa/imagecollection/dao/ImageRepository.java
@@ -1,17 +1,19 @@
 package pl.polsl.dsa.imagecollection.dao;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.polsl.dsa.imagecollection.model.*;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ImageRepository extends
         CrudRepository<ImageEntity, Long>,
         JpaSpecificationExecutor<ImageEntity> {
-    public List<ImageEntity> getAllBySize(Integer size);
-    public ImageEntity getById(Long id);
-
+    @Query("select i from ImageEntity i inner join i.categories c where c.id = :id")
+    Set<ImageEntity> findAllByCategoryId(@Param("id") Long categoryId);
 }
